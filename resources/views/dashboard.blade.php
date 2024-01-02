694f4d31bf8eb06c3695e282cff0c167abe93912
indra <mindragunawan1701@gmail.com>
2024-01-02 22:51:59 +0700

    Tambah Content dashboard admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,45 @@
 @section('contents')
     <div class="row">
         <div class="col-md-12">
-            <h1>Dashboard</h1>
-            <hr>
-            {{-- <h3>Welcome {{ Auth::user()->name }}</h3> --}}
-        </div>
-    </div>
+            <!-- Content Row -->
+            <div class="row">
 
-@endsection
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                        Status Kos (Tersedia)</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-building fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-danger shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                        Status Kos (Tidak Tersedia)</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas  fa-building fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    @endsection
--- resources/views/layouts/landingpage.blade.php
+++ resources/views/layouts/landingpage.blade.php
@@ -49,6 +49,11 @@
 </head>
 
 <body>
+    <!-- Link ke script JavaScript (opsional) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 
     <!-- Bagian Header -->
     <header>
@@ -68,10 +73,7 @@
     </footer>
 
 
-    <!-- Link ke script JavaScript (opsional) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 
 </body>
 
